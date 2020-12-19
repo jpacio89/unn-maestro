ba9209dfbf8830d4ccfafd5dadb762edcfc1680f
Joao Coelho <joao.coelho@bontouch.com>
2020-12-19 22:07:47 +0000

    no message

--- src/main/java/com/unn/maestro/transformers/temporal/TransformerRuntime.java
+++ src/main/java/com/unn/maestro/transformers/temporal/TransformerRuntime.java
@@ -12,12 +12,13 @@ import retrofit2.Call;
 
 import java.io.IOException;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class TransformerRuntime {
     MultiplesHashMap<String, Row> rowContainer;
     HashMap<String, MemoryHolder> holders;
     Transformer transformer;
-    List<String> tNamespaces;
+    MultiplesHashMap<String, String> tNamespaces;
 
     public TransformerRuntime(Transformer transformer) {
         this.transformer = transformer;
@@ -54,7 +55,13 @@ public class TransformerRuntime {
                         int primer = Integer.parseInt(row.getValues()[1]);
                         maxPrimer = Math.max(primer, maxPrimer);
 
-                        for (String tNamespace : this.tNamespaces) {
+                        for (String tNamespace : this.tNamespaces.keys()) {
+                            ArrayList<String> upstreamNamespaces = this.tNamespaces.get(tNamespace);
+
+                            if (!upstreamNamespaces.contains(namespace)) {
+                                continue;
+                            }
+
                             // TODO: ignore if transformed primer already added to the pool
                             Pair<Integer, Row> item = this.transformer.process(tNamespace, primer);
 
@@ -72,7 +79,7 @@ public class TransformerRuntime {
                         holder.setMaxProcessedTime(maxPrimer);
                     }
 
-                    for (String tNamespace : tNamespaces) {
+                    for (String tNamespace : tNamespaces.keys()) {
                         this.processDataset(tNamespace);
                     }
                 }
@@ -94,12 +101,16 @@ public class TransformerRuntime {
         }
 
         DatasetDescriptor tDescriptor = this.transformer.getDescriptor(tNamespace);
-
         Body body = new Body().withRows(rows.stream().toArray(Row[]::new));
         Dataset tDataset = new Dataset()
             .withBody(body)
             .withDescriptor(tDescriptor);
         NetworkUtils.uploadDataset(tDataset);
+    }
+
+    public ArrayList<String> getFeatures(String namespace) {
+        return holders.get(namespace).getFeatures().stream()
+            .collect(Collectors.toCollection(ArrayList::new));
     }
 
     private Dataset getNamespaceData(String namespace, int startTime) {
